5d65c97701c412392b582e71fff8a53ce8be512e
DrusMarkonii <andrey8566ok@gmail.com>
2021-12-13 13:45:13 +0300

    feat: create dynamic list

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,13 +1,27 @@
 import React, {useState} from 'react'
-import ClassCounter from './components/ClassCounter'
+import PostList from './components/PostList'
+
+import './styles/App.css'
 
 function App() {
-    const [value, setValue] = useState('Текст в импуте');
+    const [posts, setPosts] = useState([
+      {id: 1, title: 'JavaScript', content: 'Description'},
+      {id: 2, title: 'JavaScript', content: 'Description'},
+      {id: 3, title: 'JavaScript', content: 'Description'}
+    ]);
+
+    const [posts2, setPosts2] = useState([
+      {id: 1, title: 'Ruby', content: 'Description'},
+      {id: 2, title: 'Ruby', content: 'Description'},
+      {id: 3, title: 'Ruby', content: 'Description'}
+    ]);
     
 
   return (
-    <div className="App">
-      <ClassCounter />
+    <div className='App'>
+      <PostList posts={posts} title='Posts about JavaScript'/>
+      <PostList posts={posts2} title='Posts about Ruby'/>
+     
     </div>
   );
 }
